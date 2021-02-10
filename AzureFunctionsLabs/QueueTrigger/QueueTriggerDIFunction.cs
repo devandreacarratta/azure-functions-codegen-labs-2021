d0cd4a6b3f0e4b7d2a63796c7a0abb5e45208b93
Andrea Carratta <github@devandreacarratta.it>
2021-02-10 21:49:50 +0100

    Queue

---
+++ AzureFunctionsLabs/QueueTrigger/QueueTriggerDIFunction.cs
@@ -0,0 +1,23 @@
+﻿using AzureFunctionsLabs.DTO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace AzureFunctionsLabs
+{
+    public class QueueTriggerDIFunction
+    {
+        private readonly QueueTriggerDTO _dto;
+
+        public QueueTriggerDIFunction(QueueTriggerDTO dto)
+        {
+            _dto = dto;
+        }
+
+
+        [FunctionName("QueueTriggerDIFunction")]
+        public static void Run([QueueTrigger("messages", Connection = "queueconnectionstring")] string myQueueItem, ILogger log)
+        {
+            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+        }
+    }
+}
